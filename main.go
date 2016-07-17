2baf18a2a39bf7d7f4da74939f4ca41cfe78e372
Alexander Zhebrak <fata2ex@gmail.com>
2016-07-18 02:27:03 +0300

    minor fixes

--- main.go
+++ main.go
@@ -7,32 +7,28 @@ import (
 	"time"
 )
 
-var (
-	c = make(chan time.Duration, 10)
-)
+var c = make(chan time.Duration, 10)
 
 func ping(url string, number int) {
 	for i := 0; i < number; i++ {
-		t := time.Now()
+		start := time.Now()
 		resp, err := http.Get(url)
 		if err == nil && resp.StatusCode == 200 {
-			c <- time.Since(t)
+			c <- time.Since(start)
 		} else {
 			c <- 0
 		}
 	}
-
 }
 
 func main() {
 	start := time.Now()
-
 	concurrency := flag.Int("c", 10, "concurrency")
 	number := flag.Int("n", 50, "number of requests")
 	flag.Parse()
 
 	url := flag.Arg(0)
-	fmt.Println("Site:", url, "\nConcurrency:", *concurrency, "\nNumber of requests:", *number)
+	fmt.Printf("Site: %s\nConcurrency: %d\nNumber of requests: %d\n", url, *concurrency, *number)
 
 	for i := 0; i < *concurrency; i++ {
 		go ping(url, *number / *concurrency)
@@ -43,32 +39,27 @@ func main() {
 	durations := make([]time.Duration, 0, *number)
 
 	for {
-		select {
-		case duration := <-c:
-			if duration != 0 {
-				durations = append(durations, duration)
-			} else {
-				errors++
+		duration := <-c
+		if duration != 0 {
+			durations = append(durations, duration)
+		} else {
+			errors++
+		}
+
+		if len(durations)+errors == *number {
+			var sum, avg float64
+			for _, val := range durations {
+				sum += float64(val / time.Millisecond)
 			}
 
-			if len(durations)+errors == *number {
-				sum := 0.0
-				for _, val := range durations {
-					sum += float64(val / time.Millisecond)
-				}
-
-				var avg float64
-				if len(durations) != 0 {
-					avg = sum / float64(len(durations))
-				} else {
-					avg = 0
-				}
-
-				fmt.Printf("Avg: %dms Errors: %d\n", int(avg), errors)
-				fmt.Printf("Total time: %dms\n", int(time.Since(start)/time.Millisecond))
-
-				return
+			if len(durations) != 0 {
+				avg = sum / float64(len(durations))
 			}
+
+			fmt.Printf("Average response time: %dms Errors: %d\n", int(avg), errors)
+			fmt.Printf("Total time: %dms\n", int(time.Since(start)/time.Millisecond))
+
+			return
 		}
 	}
 }
